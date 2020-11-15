fd37145a8162f20bea9fa7d4a1b3dd432949f511
hzjjg <hzjio@outlook.com>
2020-11-15 22:37:22 +0800

    add: 加入定时切换单词的逻辑

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
 from dict import query
 from draw import draw
+import random
+from threading import Timer
 
-word = query('police')
+simple_words = [
+    'good',
+    'police',
+    'nice',
+    'set',
+    'right'
+]
 
-draw(word)
+random.shuffle(simple_words)
+word_index = 0
+interval = 10
+
+
+def show_word():
+    global word_index
+    word = query(simple_words[word_index])
+
+    if(word_index + 1 < len(simple_words)):
+        word_index += 1
+    else:
+        word_index = 0
+
+    draw(word)
+    t = Timer(interval, show_word)
+    t.start()
+
+
+show_word()
