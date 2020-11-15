a94536e846e61ba962fec009e3fc5dc671c471e2
hzjjg <hzjio@outlook.com>
2020-11-15 14:02:02 +0800

    update: 实现屏幕绘制音标与中文显示

--- src/draw.py
+++ src/draw.py
@@ -4,6 +4,13 @@
 
 from PIL import Image, ImageDraw, ImageFont
 
+word = {
+    'spell': 'avenger',
+    'meaning': '复仇者',
+    'phonemes': '[/əˈven.dʒɚ/]',
+    'form': 'n.'
+}
+
 img = Image.new('1', (250, 122), 255)
 size = {
     'w': 250,
@@ -22,12 +29,23 @@ draw.line(
         (1, 1)
     ), fill=0)
 
+# 单词字体
 word_font = ImageFont.truetype('arial.ttf', 28)
-draw.text((8, 12), 'avenger', font=word_font, fill=0,)
+# 单词释义字体
+explain_font = ImageFont.truetype('arial.ttf', 16)
+# 单词翻译中文字体
+meaning_font = ImageFont.truetype('msyh.ttc', 14)
 
-draw.line((8, 50, 100, 50), fill=0)
+draw.text((8, 12), word['spell'], font=word_font, fill=0,)
+draw.line((8, 50, 150, 50), fill=0)
 
-explain_font = ImageFont.truetype('arial.ttf', 20)
-draw.text((8, 55), 'n.复仇者[/əˈven.dʒɚ/]', font=explain_font, fill=0,)
+x = 8
+draw.text((x, 55), word['form'], font=explain_font, fill=0,)
+x += explain_font.getsize(word['form'])[0]
+
+draw.text((x, 55), word['meaning'], font=meaning_font, fill=0,)
+x += meaning_font.getsize(word['meaning'])[0]
+
+draw.text((x, 55), word['phonemes'], font=explain_font, fill=0,)
 
 img.show()
