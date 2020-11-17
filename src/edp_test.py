3affc5f82d2082da7bfca7f121765b768e6dfe90
hzjjg <hzjio@outlook.com>
2020-11-17 21:59:49 +0800

    test: test screen

--- src/edp_test.py
+++ src/edp_test.py
@@ -1,7 +1,6 @@
-import lib.edp.edp as edp
+import logging
 from draw import draw
-import time
-stimport logging
+import lib.edp.edp as Edp
 
 img = draw({
     'word': 'good'
@@ -10,9 +9,16 @@ img = draw({
 try:
     logging.info("Demo")
 
-    epd = edp.EPD()
+    epd = Edp.EPD()
     logging.info("init and Clear")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
 
     epd.display(epd.getbuffer(img))
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    Edp.epdconfig.module_exit()
+    exit()
